1cb5eed34a7e2371313fcd4af21a8c759c784eca
harinatarajan <harinatarajan@users.noreply.github.com>
2016-07-17 12:07:49 -0700

    Update run.sh
    Changed the properties file to include elasticsearch.

--- run.sh
+++ run.sh
@@ -49,12 +49,15 @@ wait_for_startup Cassandra \
 
 # use cassandra backed db instead of berkeleyje
 sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
-sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" conf/gremlin-server/gremlin-server.yaml
+sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-es.properties/g" conf/gremlin-server/gremlin-server.yaml
 sed -i "s/channelizer: org.apache.tinkerpop.gremlin.server.channel.WebSocketChannelizer/channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer/g" conf/gremlin-server/gremlin-server.yaml
 
 # create the backing file
 echo "gremlin.graph=com.thinkaurelius.titan.core.TitanFactory
 storage.backend=cassandra
-storage.hostname=$CASSANDRA_PORT_9160_TCP_ADDR" > conf/gremlin-server/titan-cassandra-server.properties
+storage.hostname=$CASSANDRA_PORT_9160_TCP_ADDR
+index.search.backend=elasticsearch
+index.search.hostname=$ELASTICSEARCH_PORT_9200_TCP_ADDR
+index.search.elasticsearch.client-only=false" > conf/gremlin-server/titan-cassandra-server.properties
 
 $BIN/gremlin-server.sh conf/gremlin-server/gremlin-server.yaml
